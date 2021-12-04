4f1f75b4e55d2f00ac91f8f3b9196d10fa715cee
Daniel Perez <daniel@perez.sh>
2021-12-04 19:52:55 +0000

    Cleanup

--- src/main/scala/sh/perez/advent21/Day3.scala
+++ src/main/scala/sh/perez/advent21/Day3.scala
@@ -16,14 +16,13 @@ object Day3 extends Day {
 
   def binToDec(value: Iterable[Int]): Int = value.foldLeft(0) { case (acc, v) => acc * 2 + v }
 
-  def getValue(countedBits: BitCount, mostCommon: Boolean): Int = {
-    val bits = countedBits.toList.sortBy(_._1).map { (_, v) =>
+  def getValue(countedBits: BitCount, mostCommon: Boolean): Iterable[Int] = {
+    countedBits.toList.sortBy(_._1).map { (_, v) =>
       if (mostCommon == v(0) > v(1)) 0 else 1
     }
-    binToDec(bits)
   }
 
-  def findValue(lines: Iterable[String], mostCommon: Boolean): Int = {
+  def findValue(lines: Iterable[String], mostCommon: Boolean): Iterable[Int] = {
     def doFind(index: Int, remainingLines: Iterable[String]): String = {
       if (remainingLines.size == 1) return remainingLines.head
       val count = countBits(remainingLines)(index)
@@ -32,19 +31,19 @@ object Day3 extends Day {
       doFind(index + 1, remainingLines.filter(v => v(index).asDigit == toKeep))
     }
     val remainingLine = doFind(0, lines)
-    binToDec(remainingLine.map(_.asDigit))
+    remainingLine.map(_.asDigit)
   }
 
-  def solve(f: Iterable[String] => Int): Int = {
+  def solve(f: (Iterable[String], Boolean) => Iterable[Int]): Int = {
     val lines = Source.fromResource("input3.txt").getLines().toList
-    f(lines)
+    List(true, false).map(b => binToDec(f(lines, b))).product
   }
 
   def solveFirst(): Int = {
-    solve(lines => List(true, false).map(getValue(countBits(lines), _)).product)
+    solve((lines, b) => getValue(countBits(lines), b))
   }
 
   def solveSecond(): Int = {
-    solve(lines => List(true, false).map(findValue(lines, _)).product)
+    solve(findValue)
   }
 }
