6ef71fe6781af49163013ec0653d699f79075e2a
Daniel Perez <daniel@perez.sh>
2021-12-28 23:38:44 +0000

    Use A-star instead of Dijkstra for day 15

--- build.sbt
+++ build.sbt
@@ -6,8 +6,6 @@ scalaVersion := "3.1.0"
 
 libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "2.1.0"
 
-libraryDependencies += "edu.stanford.nlp" % "stanford-corenlp" % "4.3.2"
-
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.9" % Test
 
 Test / logBuffered := false
--- src/main/scala/sh/perez/advent21/Day15.scala
+++ src/main/scala/sh/perez/advent21/Day15.scala
@@ -1,71 +1,82 @@
 package sh.perez.advent21
 
-import scala.collection.immutable.ListSet
-import scala.collection.immutable.Queue
-import edu.stanford.nlp.util.PriorityQueue
-import edu.stanford.nlp.util.BinaryHeapPriorityQueue
+import scala.collection.mutable.PriorityQueue
 
 object Day15 extends Day {
-  type Point = (Int, Int)
   val day = 15
 
   lazy val riskMap = {
-    inputLines().zipWithIndex.flatMap((line, y) => {
-      line.zipWithIndex.map((c, x) => ((x, y), c.asDigit))
-    }).toMap
+    Grid(inputLines().zipWithIndex.flatMap((line, y) => {
+      line.zipWithIndex.map((c, x) => (Point2D(x, y), c.asDigit))
+    }).toMap)
   }
 
   lazy val fullMap = {
-    val (maxX, maxY) = getEnd(riskMap)
+    val Point2D(maxX, maxY) = riskMap.end
     def computeRisk(value: Int): Int = if (value >= 10) (value % 10) + 1 else value
-    riskMap.foldLeft(Map.empty[Point, Int]) { case (map, ((x, y), risk)) =>
+    Grid(riskMap.points.foldLeft(Map.empty[Point2D, Int]) { case (map, (Point2D(x, y), risk)) =>
       (0 until 5).foldLeft(map) { case (m, dx) => {
         (0 until 5).foldLeft(m) { case (m, dy) => {
-          m + ((x + (maxX + 1) * dx, y + (maxY + 1) * dy) -> computeRisk(risk + dx + dy))
+          m + (Point2D(x + (maxX + 1) * dx, y + (maxY + 1) * dy) -> computeRisk(risk + dx + dy))
         }}
       }}
+    })
+  }
+
+  case class Grid(points: Map[Point2D, Int]) {
+    val end = Point2D(points.keys.map(_.x).max, points.keys.map(_.y).max)
+    def apply(p: Point2D): Int = points(p)
+
+    def distanceToEnd(p: Point2D): Int = (p.x - end.x).abs + (p.y - end.y).abs
+
+    def getNeighbors(p: Point2D): Set[Point2D] = {
+      Set((p.x - 1, p.y), (p.x + 1, p.y), (p.x, p.y - 1), (p.x, p.y + 1)).
+        map((x, y) => Point2D(x, y)).filter(points.contains)
     }
   }
 
-  def getEnd(map: Map[Point, Int]): Point = (map.keys.map(_._1).max, map.keys.map(_._2).max)
+  case class PointWithPriority(priority: Int, point: Point2D)
 
-  def getNeighbors(p: Point, map: Map[Point, Int]): Set[Point] = {
-    val (x, y) = p
-    Set((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)).filter(map.contains)
+  implicit val pointOrdering: Ordering[PointWithPriority] = new Ordering[PointWithPriority] {
+    override def compare(a: PointWithPriority, b: PointWithPriority) = b.priority.compare(a.priority)
   }
 
-  def expandRiskMap(map: Map[Point, Int]): Map[Point, Int] = {
-    def expand(queue: PriorityQueue[Point], expanded: Map[Point, Int]): Map[Point, Int] = {
-      if (queue.isEmpty()) return expanded
-      val point = queue.removeFirst()
-      val neighbors = getNeighbors(point, map)
-      val newMap = neighbors.foldLeft(expanded) { case (ex, p) => {
-        val nv = ex(point) + map(p)
-        if (ex.get(p).map(nv < _).getOrElse(true)) {
-          queue.changePriority(p, -nv)
-          ex + (p -> nv)
-        } else ex
-      } }
-      expand(queue, newMap)
+  def expandRiskMap(grid: Grid): Grid = {
+    val start = Point2D(0, 0)
+    val queue = new PriorityQueue[PointWithPriority]()
+    queue.enqueue(PointWithPriority(0, start))
+
+    def expand(points: Set[Point2D], gScore: Map[Point2D, Int], fScore: Map[Point2D, Int]): Map[Point2D, Int] = {
+      if (queue.isEmpty) return gScore
+      val node = queue.dequeue()
+      if (node.point == grid.end) return gScore
+      val (newGScore, newFScore, newPoints) = grid.getNeighbors(node.point)
+        .map(p => (p, gScore(node.point) + grid(p)))
+        .filter((p, g) => !gScore.contains(p) || g < gScore(p)).foldLeft((gScore, fScore, points)) {
+          case ((gs, fs, ps), (p, g)) => {
+            val f = g + grid.distanceToEnd(p)
+            if (!ps.contains(p)) queue.enqueue(PointWithPriority(f, p))
+            (gs + (p -> g), fs + (p -> f), ps + p)
+          }
+        }
+      expand(points - node.point, newGScore, newFScore)
     }
-    val priorityQueue = new BinaryHeapPriorityQueue[Point]()
-    priorityQueue.add((0, 0), 0)
-    expand(priorityQueue, Map.empty[(Int, Int), Int] + ((0, 0) -> 1))
+    Grid(expand(Set(start), Map(start -> 0), Map(start -> grid.distanceToEnd(start))))
   }
 
-  def reconstructPath(expandedMap: Map[Point, Int]): List[Point] = {
-    def reconstruct(point: Point, path: List[Point]): List[Point] = {
-      if (point == (0, 0)) return path
-      val newPoint = getNeighbors(point, expandedMap).minBy(expandedMap)
+  def reconstructPath(expandedMap: Grid): List[Point2D] = {
+    def reconstruct(point: Point2D, path: List[Point2D]): List[Point2D] = {
+      if (point == Point2D(0, 0)) return path
+      val newPoint = expandedMap.getNeighbors(point).minBy(v => expandedMap(v))
       reconstruct(newPoint, point :: path)
     }
-    reconstruct(getEnd(expandedMap), Nil)
+    reconstruct(expandedMap.end, Nil)
   }
 
-  def solve(map: Map[Point, Int]): Int = {
-    val expandedRiskMap = expandRiskMap(map)
+  def solve(grid: Grid): Int = {
+    val expandedRiskMap = expandRiskMap(grid)
     val path = reconstructPath(expandedRiskMap)
-    path.map(map).sum
+    path.map(v => grid(v)).sum
   }
 
   def solveFirst(): Int = solve(riskMap)
