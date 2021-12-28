5e797b2dc5d5434f9e2b1b4356123649bdcd5006
Daniel Perez <daniel@perez.sh>
2021-12-28 21:15:50 +0000

    Speed up day 24

--- src/main/scala/sh/perez/advent21/Day24.scala
+++ src/main/scala/sh/perez/advent21/Day24.scala
@@ -10,34 +10,35 @@ object Day24 extends Day {
     (z / n2) * (25 * x + 1) + (w + n3) * x
   }
 
+  def reverseStep(w: Int, z2: Int, n1: Int, n2: Int, n3: Int): List[Int] = {
+    // case x = 0: z in [z2 * n2, (z2 + 1) * n2)
+    val casesX0 = (z2 * n2 until (z2 + 1) * n2).toList
+    // case x = 1: z in [n2 * (z2 - w - n3) / 26, n2 * ((z2 - w - n3) / 26 + 1))
+    val caseX1Threshold = n2 * (z2 - w - n3) / 26
+    val casesX1 = (caseX1Threshold until caseX1Threshold + n2).toList
+    (casesX0 ++ casesX1).filter(step(w, _, n1, n2, n3) == z2)
+  }
+
   def extractVars(instrs: List[String]): (Int, Int, Int) = {
     val List(n1, n2, n3) = List(5, 4, 15).map(i => instrs(i).split(" ").last.toInt)
     (n1, n2, n3)
   }
 
-  // very naive and inefficient but finishes in ~5m on a normal laptop
-  def findPossibilities(): List[Long] = {
-    val grouped = inputLines().toList.grouped(18).toList
-    val zss = grouped.dropRight(1).scanLeft(Set(0)) { case (zs, group) =>
-      val (n1, n2, n3) = extractVars(group)
-      (1 to 9).flatMap(w => zs.map(z => step(w, z, n1, n2, n3))).toSet
-    }
+  lazy val programs = inputLines().toList.grouped(18).toList
 
-    val recovered = zss.zip(grouped).reverse.zipWithIndex.foldLeft(List((0L, 0))) { case (acc, ((zs, group), i)) => {
-      val acceptable = acc.map(_._2).toSet
-      val (n1, n2, n3) = extractVars(group)
-      (1 to 9).flatMap(w => {
-        zs.map(z => (z, step(w, z, n1, n2, n3))).filter((z, z2) => acceptable.contains(z2)).flatMap((z, z2) => {
-          val existing = acc.filter(_._2 == z2)
-          existing.map((i0, _) => (i0 + w.toLong * math.pow(10, i).toLong, z))
-        })
-      }).toList
+  def computePrevious(zs: Map[Int, Set[Long]], index: Int): Map[Int, Set[Long]] = {
+    val (n1, n2, n3) = extractVars(programs(index))
+    (1 to 9).foldLeft(Map.empty[Int, Set[Long]].withDefaultValue(Set.empty[Long])) { (acc, w) => {
+      zs.foldLeft(acc) { case (m, (z2, ws)) =>
+        reverseStep(w, z2, n1, n2, n3).foldLeft(m) { case (m, z) =>
+          m + (z -> (m(z) ++ ws.map(wt => wt + w.toLong * math.pow(10, programs.length - (index + 1)).toLong)))
+        }
+      }
     } }
-
-    recovered.map(_._1)
   }
 
-  lazy val possibilities = findPossibilities()
+  // fairly naive with exponential complexity but runs in ~1m on an i7 laptop
+  lazy val possibilities = (programs.length - 1 to 0 by -1).foldLeft(Map(0 -> Set(0L)))(computePrevious)(0)
 
   def solveFirst(): Long = possibilities.max
 
